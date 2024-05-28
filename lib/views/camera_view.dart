584788c35c8ec76b3bb4fc5471258fba3513d3bf
Lina Marcela Rivas <70968546+atardecer01@users.noreply.github.com>
2024-05-27 22:44:30 -0500

    Update camera_view.dart
    texto en pantalla

--- lib/views/camera_view.dart
+++ lib/views/camera_view.dart
@@ -60,26 +60,25 @@ class CameraView extends StatelessWidget {
             children: [
               CameraPreview(controller.cameraController),
               Positioned(
-                top: (controller.y) * 700,
-                right: (controller.x) * 500,
+                top: 20,
+                right: 20,
                 child: Container(
-                  width: controller.w * 100 * context.width / 100,
-                  height: controller.h * 100 * context.height / 100,
+                  width:  100 ,
+                  height: 100 ,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(8),
                     border: Border.all(color: Colors.deepPurple, width: 4.0 ),
                 
                   ),
-                 child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      color: Colors.white,
-                      child: Text(controller.label),
-                    )
-                    ,
-                  ],
-                  ), 
+                 child: Center(
+                    child: Text(
+                      '${scanController.labelf}',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 10, 10, 10), // Color del texto
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
                 ),
               )
             ],
