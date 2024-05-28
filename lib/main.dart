54e91de2b92ea584ac67bf5e60f8047e5f166342
Marcela <rivas.lina@correounivalle.edu.co>
2024-05-27 21:33:10 -0500

    arreglo de errores

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:ejemplo/views/camera_view.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,22 +12,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   // This widget is the root of your application.
-  @override
-  _MainAppState createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  ThemeMode _themeMode = ThemeMode.light;
-
-  void _toggleTheme() {
-    setState(() {
-      _themeMode =
-          _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,10 +21,8 @@ class _MainAppState extends State<MainApp> {
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-
       ),
       home: const CameraView(),
     );
   }
 }
-
