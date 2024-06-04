e817f57291504759b8bc77259b71dd54b9167590
Lina <rivas.lina@correounivalle.edu.co>
2024-06-04 17:51:03 -0500

    cambio de modelo

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -72,8 +72,8 @@ class ScanController extends GetxController {
 
   initTFLite() async{
     await Tflite.loadModel(
-      model: "assets/model.tflite",
-      labels: "assets/labels.txt",
+      model: "assets/yolov2_tiny.tflite",
+      labels: "assets/yolov2_tiny.txt",
       isAsset: true,
       numThreads: 1,
       useGpuDelegate: false,
